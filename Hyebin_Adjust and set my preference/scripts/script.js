dc007cc47bac98009d8a569a8e5250b27484739d
hyebinleee <hyebin22222@gmail.com>
2023-11-09 22:28:27 -0800

    created database of preferences

--- Hyebin_Adjust and set my preference/scripts/script.js
+++ Hyebin_Adjust and set my preference/scripts/script.js
@@ -1,57 +1,97 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var searchButton = document.getElementById("search");
-    if (searchButton) {
-        searchButton.onclick = function () {
-            location.href = "result.html";
-        }
-    }
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    apiKey: "AIzaSyAubQD94XEVOJpl--IpCtsrrJhbiyiDtXc",
+    authDomain: "comp1800-bby34.firebaseapp.com",
+    projectId: "comp1800-bby34",
+    storageBucket: "comp1800-bby34.appspot.com",
+    messagingSenderId: "337346687619",
+    appId: "1:337346687619:web:5d9cdc94c326f0058361b6",
+    measurementId: "G-G9TZPNE0Y5"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+document.getElementById("search").addEventListener("click", function () {
+    // Get user preferences from checkboxes, radio buttons, sliders, etc.
+    var bestMatch = document.getElementById("bestmatch-checkbox").checked;
+    var mostPopular = document.getElementById("mostpopular-checkbox").checked;
+    var closest = document.getElementById("closest-checkbox").checked;
+    var distance = document.getElementById("myRange").value;
+    var hygieneRating = document.querySelector('input[name="rate"]:checked').value;
+    var storageBin = document.getElementById("storagebin-checkbox").checked;
+    var wheelchair = document.getElementById("wheelchair-checkbox").checked;
+    var waterFountain = document.getElementById("waterfountain-checkbox").checked;
+    var airPump = document.getElementById("airpump-checkbox").checked;
+
+    // Create an object with user preferences
+    var userPreferences = {
+        bestMatch: bestMatch,
+        mostPopular: mostPopular,
+        closest: closest,
+        distance: distance,
+        hygieneRating: hygieneRating,
+        storageBin: storageBin,
+        wheelchair: wheelchair,
+        waterFountain: waterFountain,
+        airPump: airPump
+    };
+
+    // Call a function to store preferences in Firebase
+    storeUserPreferences(userPreferences);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    var storageBin = document.getElementById("storagebin-checkbox");
-    if (storageBin) {
-        storageBin.onclick = function () {
-            console.log("User selected storage bin option.");
+function storeUserPreferences(preferences) {
+    // Add a new document with user preferences to the "preferences" collection
+    db.collection("preferences").add(preferences)
+        .then(function (docRef) {
+            console.log("Preferences document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding preferences: ", error);
+        });
+}
+
+function getUserPreferences() {
+    // Read all documents from the "preferences" collection
+    db.collection("preferences").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            // Handle the retrieved preferences as needed
+        });
+    });
+}
+
+// Select all input elements with the class name "rates" and store them in the "stars" variable
+const stars = document.querySelectorAll('.rate input');
+
+// Function to update star icons based on the selected rating
+function updateStars(index) {
+    stars.forEach((star, i) => {
+        const label = document.querySelector(`.rate label[for="star${i + 1}"]`);
+        if (i <= index) {
+            label.innerHTML = 'star';
+        } else {
+            label.innerHTML = 'star_border';
         }
-    }
+    });
+}
+
+// Iterate through each star element
+stars.forEach((star, index) => {
+    // Add a click event listener to the current star
+    star.addEventListener('click', () => {
+        // Update star icons based on the clicked star
+        updateStars(index);
+    });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    var wheelchair = document.getElementById("wheelchair-checkbox");
-    if (wheelchair) {
-        wheelchair.onclick = function () {
-            console.log("User selected wheelchair option.");
-        }
-    }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    var waterfountain = document.getElementById("waterfountain-checkbox");
-    if (waterfountain) {
-        waterfountain.onclick = function () {
-            console.log("User selected water fountain option.");
-        }
-    }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    var airpump = document.getElementById("airpump-checkbox");
-    if (airpump) {
-        airpump.onclick = function () {
-            console.log("User selected airpump option.");
-        }
-    }
-});
-
-// document.addEventListener("DOMContentLoaded", function(){
-//     var backButton = document.getElementById("back");
-//     if(back){
-//         back.onclick = function(){
-//             console.log("User clicked back button.");
-//         }   
-//     }    
-// });
-
-
-
-
+function saveAlert() {
+    alert('Your preferences are saved!');
+}
