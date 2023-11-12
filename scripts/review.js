87462d2993823ca0cb6630fa53180088a9b50162
Louise Li <louiseli.van@gmail.com>
2023-11-11 23:13:34 -0800

    created review database. document displays the washroomid,userid, review text, review star rating, and a timestamp. also redirects to review submission page (in development, not done)

--- scripts/review.js
+++ scripts/review.js
@@ -1,2 +1,55 @@
-var hikeDocID = localStorage.getItem("hikeDocID");    //visible to all functions on this page
+var washroomDocID = localStorage.getItem("washroomDocID");    //visible to all functions on this page
 
+
+// for below, need to wait for dan to finish washroom list and to create initial list
+
+// function getWashroomName(id) {
+//     db.collection("washrooms")
+//         .doc(id)
+//         .get()
+//         .then((thisWashroom) => {
+//             var washroomName = thisWashroom.data().name;
+//             document.getElementById("washroom-name").innerHTML = washroomName;
+//                 });
+// }
+
+// getWashroomName(washroomDocID);
+
+function writeReview() {
+    console.log("inside write review")
+    let reviewText = document.getElementById("text-box-area").value;
+
+    // star rating
+    const stars = document.querySelectorAll('star');
+
+    let reviewRating = 0;
+
+    stars.forEach((star) => {
+        if (star.textContent === 'star') {
+            reviewRating++;
+        }
+    });
+
+    console.log(reviewText, reviewRating);
+
+    var user = firebase.auth().currentUser;
+    if (user) {
+        var currentUser = db.collection("users").doc(user.uid);
+        var userID = user.uid;
+
+        db.collection("reviews").add({
+            washroomDocID: washroomDocID,
+            userID: userID,
+            reviewText: reviewText,
+            rating: reviewRating,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(() => {
+            // redirects to review submission page
+            window.location.href = "review-submission-successful";
+        });
+    } else {
+        console.log("No user is signed in");
+        window.location.href = 'review.html';
+    }
+}        
+writeReview();
