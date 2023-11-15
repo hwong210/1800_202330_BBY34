2883c9bed7a91685a4bfae4c980c4141229155ea
Taeyang Seo    <danielseo354@gmail.com>
2023-11-14 17:30:33 -0800

    adding changes to addlocation.html for star rating system

--- scripts/addlocation.js
+++ scripts/addlocation.js
@@ -2,10 +2,34 @@ function submitWashroom() {
     console.log("inside submit washroom");
     let washroomName = document.getElementById("text-field").value;
     let washroomAddress = document.getElementById("text-field-two").value;
+ 
+    const stars = document.querySelectorAll('.star');
+		
+    
+    let hygieneLevel = 0;
+		
+    
+    stars.forEach((star) => {
+				
+        if (star.textContent === 'star') {
+						
+            hygieneLevel++;
+        }
+    });
+    
+    let safetyLevel = 0;
+		
+    
+    stars.forEach((star) => {
+				
+        if (star.textContent === 'star') {
+						
+            safetyLevel++;
+        }
+    });
     
     
-    
-    console.log(washroomName, washroomAddress);
+    console.log(washroomName, washroomAddress, safetyLevel, hygieneLevel);
 
     var user = firebase.auth().currentUser;
     if (user) {
@@ -18,12 +42,28 @@ function submitWashroom() {
             userID: userID,
             name: washroomName,
             address: washroomAddress,
+            safety: safetyLevel,
+            hygiene: hygieneLevel,
             timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }).then(() => {
-            window.location.href = "addpreference.html"; // Redirect to the thanks page
+            window.location.href = "thanks.html"; // Redirect to the thanks page
         });
     } else {
         console.log("No user is signed in");
         window.location.href = 'addlocation.html';
     }
 }
+
+const stars = document.querySelectorAll('.star');
+
+// Iterate through each star element
+stars.forEach((star, index) => {
+    // Add a click event listener to the current star
+    star.addEventListener('click', () => {
+        // Fill in clicked star and stars before it
+        for (let i = 0; i <= index; i++) {
+            // Change the text content of stars to 'star' (filled)
+            document.getElementById(`star${i + 1}`).textContent = 'star';
+        }
+    });
+});
