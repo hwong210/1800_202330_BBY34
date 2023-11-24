3c1b544d3716e8394154210be7f6967d97c5cf9b
hason wong <hasonwong92@gmail.com>
2023-11-23 20:49:02 -0800

    added map

---
+++ scripts/index.js
@@ -0,0 +1,58 @@
+let map, infoWindow;
+
+// initMap is now async
+async function initMap() {
+  // Request libraries when needed, not in the script tag.
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  // Short namespaces can be used.
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 49.248499, lng: -123.001375 },
+    zoom: 15,
+  });
+
+  infoWindow = new InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        },
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation.",
+  );
+  infoWindow.open(map);
+}
+
+// Trigger the async initMap function
+initMap();
