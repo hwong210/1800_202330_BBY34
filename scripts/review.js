ef64db7de1339597eccaac36101d38baf38ff646
Louise <louiseli.van@gmail.com>
2023-12-05 14:44:41 -0800

    reorganizing and commenting review.js

--- scripts/review.js
+++ scripts/review.js
@@ -1,6 +1,6 @@
 var washroomDocID = localStorage.getItem("washroomDocID");    //visible to all functions on this page
 
-// works fine
+// Display washroom name on the page.
 function getWashroomName(id) {
     db.collection("washrooms")
         .doc(id)
@@ -13,57 +13,22 @@ function getWashroomName(id) {
 
 getWashroomName(washroomDocID);
 
+// Writes review and stores review values.
 function writeReview() {
-    console.log("inside write review")
+    // Initializes reviewText to the text box value. 
     let reviewText = document.getElementById("text-box-area").value;
 
-    // star rating issue below. need help. doesnt save properly
-    const stars = document.getElementsByName('rate');
+    // Initializes reviewRating to the star value.
+    let reviewRating = getStarValue('rate');
 
-    let reviewRating = 0;
+    // Initializes values to 0 or 1, depending on if specific buttons are checked.
+    let cleanValue = getValueFromCheckbox("clean-button");
+    let ventilatedValue = getValueFromCheckbox("ventilated-button");
+    let spaciousValue = getValueFromCheckbox("spacious-button");
+    let privateValue = getValueFromCheckbox("private-button");
+    let accessibleValue = getValueFromCheckbox("accessible-button");
 
-    for (let i = 0; i < stars.length; i++) {
-        if (stars[i].checked) {
-            reviewRating = parseInt(stars[i].value);
-            break;
-        }
-    }
-
-    let cleanTag = document.getElementById("clean-button");
-    // if cleantag checked then cleanvalue = 1, otherwise 0
-    let cleanValue = 0;
-    // Update cleanValue only if the checkbox is checked
-    if (cleanTag.checked) {
-        cleanValue = 1;
-    }
-
-    let ventilatedTag = document.getElementById("ventilated-button");
-    let ventilatedValue = 0;
-    if (ventilatedTag.checked) {
-        ventilatedValue = 1;
-    }
-
-    let spaciousTag = document.getElementById("spacious-button");
-    let spaciousValue = 0;
-    if (spaciousTag.checked) {
-        spaciousValue = 1;
-    }
-
-    let privateTag = document.getElementById("private-button");
-    let privateValue = 0;
-    if (privateTag.checked) {
-        privateValue = 1;
-    }
-
-    // may remove if included in addwashroom section
-    let accessibleTag = document.getElementById("accessible-button");
-    let accessibleValue = 0;
-    if (accessibleTag.checked) {
-        accessibleValue = 1;
-    }
-
-    console.log(reviewText, reviewRating);
-
+    // Authenticates user.
     var user = firebase.auth().currentUser;
     if (user) {
         var currentUser = db.collection("users").doc(user.uid);
@@ -91,4 +56,22 @@ function writeReview() {
         window.location.href = 'review.html';
     }
 
+}
+
+// Function to get the checked star values.
+function getStarValue(starId) {
+    let stars = document.getElementsByName(starId);
+    let starValue = 0;
+    for (let i = 0; i < stars.length; i++) {
+        if (stars[i].checked) {
+            starValue = parseInt(stars[i].value);
+        }
+    }
+    return starValue;
+}
+
+// Function to get review tag values.
+function getValueFromCheckbox(checkBoxId) {
+    let checkbox = document.getElementById(checkBoxId);
+    return checkbox.checked ? 1 : 0;
 }
